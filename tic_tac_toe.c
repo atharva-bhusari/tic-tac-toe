0f949922cbda83d6cc9b1a01becaa70e27698ff8
buzzi0204 <buzzi.atharva@gmail.com>
2017-08-23 19:41:22 +0530

    Add files via upload

---
+++ tic_tac_toe.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include<conio.h>
+
+
+char square[10]={'0','1','2','3','4','5','6','7','8','9'};
+
+void board();
+ int checkwin();
+
+int main()
+{     int player;
+   int count, choice;
+     char mark;
+       printf("Welcome to Tic Tac Toe Game");
+
+
+
+          do
+                {
+                board();
+
+          player=(player%2)?1:2;
+                  printf("\n Enter the number between 1 to 9 -->");
+                         scanf("%d", &choice);
+                         mark=(player==2)?'O':'X';
+
+        if(choice==1 && square[1]=='1')
+                square[1]=mark;
+
+        else if(choice==2 && square[2]=='2')
+            square[2]=mark;
+
+        else if(choice==3 && square[3]=='3')
+            square[3]=mark;
+
+        else if(choice==4 && square[4]=='4')
+            square[4]=mark;
+
+        else if(choice==5 && square[5]=='5')
+            square[5]=mark;
+
+        else if(choice==6 && square[6]=='6')
+            square[6]=mark;
+
+        else if(choice==7 && square[7]=='7')
+            square[7]=mark;
+
+        else if(choice==8 && square[8]=='8')
+            square[8]=mark;
+
+        else if(choice==9 && square[9]=='9')
+            square[9]=mark;
+        else
+            {
+                   printf("invalid move");
+        player--;
+         }
+         count=checkwin();
+               player++;
+
+
+    }while(count==-1);
+        board();
+         if(count==1)
+                printf("Player %d win\n",--player);
+         else
+            printf("Game Draw\n");
+         return 0;
+         }
+
+         void board()
+{
+	system("cls");
+	printf("\n\nWelcome to the game of TIC TAC TOE\n\n");
+
+	printf("Player 1 = X\t\tPlayer 2= O\n\n");
+
+	printf("     |     |     \n");
+	printf("   %c |   %c |   %c \n", square[1],square[2],square[3]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+
+
+	printf("   %c |   %c |   %c \n", square[4],square[5],square[6]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+
+
+	printf("   %c |   %c |   %c \n", square[7],square[8],square[9]);
+	printf("     |     |     \n");
+
+}
+
+int checkwin()
+{
+
+        if(square[1]==square[2]&&square[2]==square[3])
+                return 1;
+        else if(square[4]==square[5]&&square[5]==square[6])
+                return 1;
+        else if(square[7]==square[8]&&square[8]==square[9])
+                return 1;
+        else if(square[1]==square[4]&&square[4]==square[7])
+            return 1;
+        else if(square[2]==square[5]&&square[5]==square[8])
+           return 1;
+        else if(square[3]==square[6]&&square[6]==square[9])
+                return 1;
+        else if(square[1]==square[5]&&square[5]==square[9])
+            return 1;
+        else if(square[3]==square[5]&&square[5]==square[7])
+               return 1;
+               else if(square[1]!='1' && square[2]!='2' && square[3]!='3' && square[4]!='4' && square[5]!='5' && square[6]!= '6' && square[7]!='7' && square[8]!='8' && square[9]!= '9')
+                return 0;
+                  else
+                        return -1;
+
+	             }
